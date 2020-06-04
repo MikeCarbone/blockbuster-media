4edb848a92e5212a1e41590ead86be413278742f
Mike Carbone <mfcbone2@gmail.com>
2020-06-04 14:40:58 -0400

    :fire: Going dark

--- components/BigArticle.js
+++ components/BigArticle.js
@@ -12,11 +12,11 @@ import {
 
 const BigArticle = props => (
     <>
-      <Box className="big-article" flexDirection="column" justifyContent="left">
+      <Box className="big-article" flexDirection="column" justifyContent="left" backgroundColor="var(--black)">
         <Image height="460px" src={props.imgSrc} />
         <VertSpace />
-        <Heading size={2} color="var(--black)">{props.title}</Heading>
-        <Copy margin="5px 0 0 0" color="var(--black)">{props.subtitle}</Copy>
+        <Heading size={2}>{props.title}</Heading>
+        <Copy margin="5px 0 0 0">{props.subtitle}</Copy>
         <Button href="/article" linkWrapper={NextLink} secondary margin="10px auto 0 0">{props.buttonText}</Button>
       </Box>
       <style jsx>{``}</style>
--- components/SmallArticle.js
+++ components/SmallArticle.js
@@ -10,9 +10,9 @@ import {
 const SmallArticle = props => (
     <NextLink href="/article">
       <a style={{ width: props.width ? props.width : 'auto' }}>
-        <Box display="block">
+        <Box display="block" backgroundColor="var(--black)">
           <Image height="120px" width="100%" src={props.imgSrc} />
-          <Copy margin="5px 0 20px 0" color="var(--black)" fontWeight="400" >{props.title}</Copy>
+          <Copy margin="5px 0 20px 0" fontWeight="400" >{props.title}</Copy>
         </Box>
       </a>
     </NextLink>
